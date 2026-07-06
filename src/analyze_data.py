0386a843de0a7f28798626c739a4e4f6dfd03ca2
riko <riko.lunch@gmail.com>
2026-07-06 17:39:39 +0900

    feat: add top 10 stars chart

--- src/analyze_data.py
+++ src/analyze_data.py
@@ -1,7 +1,21 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.read_csv("data/python_repositories.csv")
+CSV_PATH = "data/python_repositories.csv"
 
-print(df.head())
+def plot_top10_stars():
+    df = pd.read_csv(CSV_PATH)
+    df.sort_values('Stars')
+    top10 = df.head(10)
 
-print(df.describe())
+    x = top10['Repository']
+    y = top10["Stars"]
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+    plt.bar(x,y)
+    plt.title('Top 10 Python Repositories by GitHub Stars')
+    plt.xlabel('Repository Name')
+    plt.ylabel('Number of Stars')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig(r'.\graphs\top10_stars.png')
