7bb0faeb6d4c2c8aa63ffcd7c1ad1f450ee7543b
riko <riko.lunch@gmail.com>
2026-07-06 16:56:20 +0900

    feat: add analysis foundation

--- src/main.py
+++ src/main.py
@@ -1,3 +1,8 @@
 from fetch_data import fetch_python_repositories
 
-fetch_python_repositories()
+def main():
+    df = fetch_python_repositories()
+    print(f"{len(df)}件のリポジトリを取得しました。")
+
+if __name__ == "__main__":
+    main()
